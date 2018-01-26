1a66f869591e39f3ad68c6df35ee30501829fca4
Dennis Mullen <dkmullen@gmail.com>
2018-01-25 19:30:54 -0500

    First commit

---
+++ es6-notes.js
@@ -0,0 +1,80 @@
+/*jshint esversion: 6 */
+
+/*
+Destructuring Arrays (1-3)
+*/
+
+const things = ['red', 'basketball', 'paperclip', 'green', 'computer', 'earth', 'udacity', 'blue', 'dogs'];
+
+const [a, b, c] = things;  // Draws out the first three items in the array
+
+/* Use destructuring to initialize the variables `one`, `two`, and `three`
+* with the colors from the `things` array.
+*/
+
+const [one, , , two, , , , three] = things; // Commas skip items in an array
+
+const colors = `List of Colors
+1. ${one}
+2. ${two}
+3. ${three}`;
+
+console.log(colors);
+
+
+// Destructuring with an object
+const gemstone = {
+  type: 'quartz',
+  color: 'rose',
+  karat: 21.29
+};
+
+const {type, color, karat} = gemstone;
+
+console.log(type, color, karat); // Prints: quartz rose 21.29
+
+// Iterate with for-of
+
+ const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+
+ for (const day of days) {
+     console.log(day[0].toUpperCase() + day.slice(1));
+ }
+
+
+/* ------------- Spread Operator
+* Instructions: Use the spread operator to combine the `fruits` and `vegetables` arrays into the `produce` array.
+*/
+
+const fruits = ["apples", "bananas", "pears"];
+const vegetables = ["corn", "potatoes", "carrots"];
+
+const produce = [...fruits, ...vegetables];
+
+console.log(produce);
+
+// The spread operator spreads an array into its parts. so...
+console.log(...fruits); // prints: apples bananas pears - no commas, brackets
+// So the spread operator is a good way to combine arrays
+
+
+/*
+ * ------------- Rest Parameter
+ */
+// The Rest parameter is kind of the opposite of spread - binds seperate items into an array
+// In the following, the function can take any number of args, and they will all be
+// combined into an array called nums
+
+function average(...nums) {
+  console.log(nums);
+  let total = 0;
+  for (const num of nums) {
+    total += num;
+  }
+  return total/nums.length || 0; // Zero covers the case of no args
+}
+
+console.log(average(2, 6));
+console.log(average(2, 3, 3, 5, 7, 10));
+console.log(average(7, 1432, 12, 13, 100));
+console.log(average());
